66837826b40a3b4449834d67949d21a675bf9ee4
tanchik <negashovata@gmail.com>
2022-09-15 08:03:57 +0300

    добавлена 5 задача

--- zadachi.py
+++ zadachi.py
@@ -55,7 +55,7 @@
 # #- для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 
 number = int(input('Введите число: '))
-def fibonachi(num):
+def fibonacсi(num):
     fibo = []
     n1, n2 = 1, 1
     for _ in range(num):
@@ -66,5 +66,4 @@ def fibonachi(num):
         fibo.insert(0, n1)
         n1, n2 = n2, n1 - n2
     return fibo
-print(fibonachi(number))
-# 
+print(fibonacсi(number))
